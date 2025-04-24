3655a4558ff878b2ed9f2f754d66aebac410f35b
fanisealetsa <159315624+fanisealetsa@users.noreply.github.com>
2025-04-25 00:07:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,21 +13,10 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        rainfall = float(request.form['rainfall'])
-        temperature = float(request.form['temperature'])
-        prediction = model.predict([[rainfall, temperature]])
-        return render_template('result.html', prediction=round(prediction[0], 2))
-    except Exception as e:
-        return render_template('result.html', prediction=f"Error: {e}")
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+    rainfall = float(request.form['rainfall'])
+    temperature = float(request.form['temperature'])
+    prediction = model.predict([[rainfall, temperature]])
+    return render_template('result.html', prediction=round(prediction[0], 2))
 
 if __name__ == '__main__':
     app.run(debug=True)
