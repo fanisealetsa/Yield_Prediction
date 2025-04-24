10559194678f124cab87f5bdd29a63128b70aeed
fanisealetsa <159315624+fanisealetsa@users.noreply.github.com>
2025-04-24 22:57:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def predict():
     return jsonify(prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get the PORT environment variable
+    app.run(host='0.0.0.0', port=port)
